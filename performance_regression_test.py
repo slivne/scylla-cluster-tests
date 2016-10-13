a9f08e5ff6c2e53c928cd2545860d1842e97c990
Amos Kong <amos@scylladb.com>
2016-10-13 10:20:43 +0800

    dev: update config
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- performance_regression_test.py
+++ performance_regression_test.py
@@ -134,8 +134,8 @@ class PerformanceRegressionTest(ClusterTester):
                     "-pop seq=1..10000000")
         base_cmd = ("cassandra-stress %s no-warmup cl=QUORUM duration=1m "
                     "-schema 'replication(factor=3)' -port jmx=6868 "
-                    "-mode cql3 native -rate threads=2 -errors ignore "
-                    "-pop seq=1..1000")
+                    "-mode cql3 native -rate threads=500 -errors ignore "
+                    "-pop seq=1..100000000")
 
         stress_modes = self.params.get(key='stress_modes', default='write')
         for mode in stress_modes.split():
--- run-aws.sh
+++ run-aws.sh
@@ -1,6 +1,9 @@
-
-sudo iptables -F
 #avocado run performance_regression_test.py:PerformanceRegressionTest.test_foo --multiplex data_dir/jenkins_conf.yaml --filter-only /run/backends/aws/us_west_1 /run/databases/scylla --filter-out /run/backends/libvirt  --show-job-log
 #avocado run performance_regression_test.py:PerformanceRegressionTest.test_simple_regression --multiplex data_dir/jenkins_conf.yaml --filter-only /run/backends/aws/us_west_1 /run/databases/scylla --filter-out /run/backends/libvirt  --show-job-log
-avocado run performance_regression_test.py:PerformanceRegressionTest.test_simple_regression --multiplex data_dir/your_config.yaml --filter-only /run/backends/aws/us_west_1 /run/databases/scylla --filter-out /run/backends/libvirt  --show-job-log
+
+# 1.4 1.3.1 1.3
+ver=$1
+
+
+avocado run performance_regression_test.py:PerformanceRegressionTest.test_simple_regression --multiplex data_dir/your_config.yaml --filter-only /run/backends/aws/us_east_1 /run/version/$ver /run/databases/scylla --filter-out /run/backends/libvirt  --show-job-log
 
